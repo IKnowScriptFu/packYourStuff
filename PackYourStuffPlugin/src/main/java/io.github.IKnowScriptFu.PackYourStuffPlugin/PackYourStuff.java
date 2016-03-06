47ef9700545130f65b0555b3f427fcb0a90e8cef
IKnowScriptFu <luciemiliano@gmail.com>
2016-03-06 15:02:42 +0100

    created the IntelliJ project

---
+++ PackYourStuffPlugin/src/main/java/io.github.IKnowScriptFu.PackYourStuffPlugin/PackYourStuff.java
@@ -0,0 +1,10 @@
+package io.github.IKnowScriptFu.PackYourStuffPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Aemilius on 06/03/2016.
+ */
+public final class PackYourStuff extends JavaPlugin{
+
+}
